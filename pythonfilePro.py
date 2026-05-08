ee4c52154e890080ca090c8485ab0fb530e44db2
Vlad <vladpochinok2010@gmail.com>
2026-05-08 18:16:50 +0000

    text dovilniy

--- pythonfilePro.py
+++ pythonfilePro.py
@@ -1,2 +1,17 @@
-from flask import *
 from html.parser import HTMLParser
+import json
+import requests
+
+def getDatafromAPI(value, currency, timespan, interval):
+    text = requests.get(f"https://api.coingecko.com/api/v3/coins/{value}/market_chart?vs_currency={currency}&days={timespan}&interval={interval}")
+    print(text.text)
+    return text.text
+
+def unJSON(file):
+    stringDict = json.loads(file)
+    print(stringDict["prices"])
+
+
+file = getDatafromAPI("bitcoin", "usd", "15", "daily")
+print()
+unJSON(file)
